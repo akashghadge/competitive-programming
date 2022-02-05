3746780c29e03be51914043b4a7ee23d8b046b6b
akash_ghadge <asghadge6@gmail.com>
2022-02-05 23:00:44 +0530

    Quora Test No question solved

---
+++ Company Test/Quora Programming Challenge/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define vll vector<ll>
+#define ll long long
+#define v vector
+#define pii pair<int, int>
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+void test_case()
+{
+    int n;
+    v<string> arr;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+        
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST;
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        test_case();
+    }
+    return 0;
+}
---
+++ Company Test/Quora Programming Challenge/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define vll vector<ll>
+#define ll long long
+#define v vector
+#define pii pair<int, int>
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+void test_case()
+{
+    int n;
+    cin >> n;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST;
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        test_case();
+    }
+    return 0;
+}
