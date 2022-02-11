72bb2aaa5dbc4e52f69c9123edc6cc663ed42cbc
akash_ghadge <asghadge6@gmail.com>
2022-02-11 12:36:41 +0530

    A2OJ l2 dmix 1 added

---
+++ A2OJ/Ladder2/d mix/50 A. Difference Row.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define ll long long
+#define v vector
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define vpi vector<pair<int, int>>
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+void sol(vi arr, int n)
+{
+    sort(arr.begin(), arr.end());
+    swap(arr[0], arr[n - 1]);
+    for (auto val : arr)
+    {
+        cout << val << " ";
+    }
+    cout << en;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST;
+    int N;
+    cin >> N;
+    vi arr(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    sol(arr, N);
+    return 0;
+}
