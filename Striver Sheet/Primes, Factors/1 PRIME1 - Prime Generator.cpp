c0e2cd8e25888bf26da7d73cddb78be558162370
akash_ghadge <asghadge6@gmail.com>
2022-05-12 14:34:24 +0530

    SCP PRIMES STARTED

---
+++ Striver Sheet/Primes, Factors/1 PRIME1 - Prime Generator.cpp
@@ -0,0 +1,101 @@
+/*
+Peter wants to generate some prime numbers for his cryptosystem. Help him! Your task is to generate all prime numbers between two given numbers!
+
+Input
+The input begins with the number t of test cases in a single line (t<=10). In each of the next t lines there are two numbers m and n (1 <= m <= n <= 1000000000, n-m<=100000) separated by a space.
+
+Output
+For every test case print all prime numbers p such that m <= p <= n, one number per line, test cases separated by an empty line.
+
+Example
+Input:
+2
+1 10
+3 5
+
+Output:
+2
+3
+5
+7
+
+3
+5
+Warning: large Input/Output data, be careful with certain languages (though most should be OK if the algorithm is well designed)
+Information
+After cluster change, please consider PRINT as a more challenging problem.
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define ll long long
+#define v vector
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define vpi vector<pair<int, int>>
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+vector<char> segmentedSieve(long long L, long long R)
+{
+    // generate all primes up to sqrt(R)
+    long long lim = sqrt(R);
+    vector<char> mark(lim + 1, false);
+    vector<long long> primes;
+    for (long long i = 2; i <= lim; ++i)
+    {
+        if (!mark[i])
+        {
+            primes.emplace_back(i);
+            for (long long j = i * i; j <= lim; j += i)
+                mark[j] = true;
+        }
+    }
+
+    vector<char> isPrime(R - L + 1, true);
+    for (long long i : primes)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
+
+/*input sections*/
+ll l, r;
+void input()
+{
+    cin >> l >> r;
+}
+void sol()
+{
+    vector<char> isPrimes = segmentedSieve(l, r);
+    ll ptr = 0;
+    for (auto ch : isPrimes)
+    {
+        if (ch == true)
+        {
+            cout << l + ptr << en;
+        }
+        ptr++;
+    }
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST;
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        input();
+        sol();
+    }
+    return 0;
+}
