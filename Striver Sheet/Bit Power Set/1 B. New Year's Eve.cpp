0032be3f1ef514a7a062aae2277b158f811b1f72
akash_ghadge <asghadge6@gmail.com>
2022-05-13 12:01:08 +0530

    SCP BIT POWER SET STARTED

---
+++ Striver Sheet/Bit Power Set/1 B. New Year's Eve.cpp
@@ -0,0 +1,82 @@
+/*
+Since Grisha behaved well last year, at New Year's Eve he was visited by Ded Moroz who brought an enormous bag of gifts with him! The bag contains n sweet candies from the good ol' bakery, each labeled from 1 to n corresponding to its tastiness. No two candies have the same tastiness.
+
+The choice of candies has a direct effect on Grisha's happiness. One can assume that he should take the tastiest ones — but no, the holiday magic turns things upside down. It is the xor-sum of tastinesses that matters, not the ordinary sum!
+
+A xor-sum of a sequence of integers a1, a2, ..., am is defined as the bitwise XOR of all its elements: , here  denotes the bitwise XOR operation; more about bitwise XOR can be found here.
+
+Ded Moroz warned Grisha he has more houses to visit, so Grisha can take no more than k candies from the bag. Help Grisha determine the largest xor-sum (largest xor-sum means maximum happiness!) he can obtain.
+
+Input
+The sole string contains two integers n and k (1 ≤ k ≤ n ≤ 1018).
+
+Output
+Output one number — the largest possible xor-sum.
+
+Examples
+inputCopy
+4 3
+outputCopy
+7
+inputCopy
+6 6
+outputCopy
+7
+Note
+In the first sample case, one optimal answer is 1, 2 and 4, giving the xor-sum of 7.
+
+In the second sample case, one can, for example, take all six candies and obtain the xor-sum of 7.
+
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define ll long long
+#define v vector
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define vpi vector<pair<int, int>>
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+/*input sections*/
+ll n, k;
+void input()
+{
+    cin >> n >> k;
+}
+ll sol()
+{
+    if (k == 1)
+        return n;
+    int i;
+    for (i = 1; i < 64; i++)
+    {
+        if (pow(2, i) > n)
+            break;
+        else if (pow(2, i) - 1 == n)
+            break;
+    }
+    return pow(2, i) - 1;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST;
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        input();
+        cout << sol() << en;
+    }
+    return 0;
+}
