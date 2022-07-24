55df88bed2fdc62841f9ecd450a0376f68884586
akash_ghadge <asghadge6@gmail.com>
2022-07-24 21:19:42 +0530

    CF R810 1 solved only

---
+++ CodeForces/810/B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define ll long long
+#define v vector
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define vpi vector<pair<int, int>>
+#define FAST                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+/*input sections*/
+int n, m;
+vi arr;
+vpi fr;
+void input()
+{
+    cin >> n >> m;
+    arr.resize(n);
+    fr.resize(m);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> fr[i].first >> fr[i].second;
+    }
+}
+int sol()
+{
+    if (m % 2 == 0)
+        return 0;
+    
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST;
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        input();
+        cout << sol() << en;
+    }
+    return 0;
+}
