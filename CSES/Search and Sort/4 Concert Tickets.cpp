140e91f7a442b78a7ec67a38ee67cceb0c816da3
akash_ghadge <asghadge6@gmail.com>
2022-07-30 00:01:44 +0530

    CSES SS 1 added

--- CSES/Search and Sort/4 Concert Tickets.cpp
+++ CSES/Search and Sort/4 Concert Tickets.cpp
@@ -1,37 +1,31 @@
 #include <bits/stdc++.h>
-using namespace std;
-#define en "\n"
-#define ll long long
-#define v vector
-#define vi vector<int>
-#define vll vector<ll>
-#define vii vector<vector<int>>
-#define pii pair<int, int>
-#define vpi vector<pair<int, int>>
-#define FAST                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);
-/*input sections*/
 
-void input()
-{
+using namespace std;
+
+//variables used for the current problem
+int n,m,h,t; multiset<int> tickets;
+
+void solve() {
+	cin >> n >> m;
+	for (int i=0;i<n;++i){
+		cin >> h; tickets.insert(h);
+	}
+	for (int i=0;i<m;++i){
+		cin >> t;
+		auto it = tickets.upper_bound(t);
+		if (it==tickets.begin()){
+			cout << -1 << "\n";
+		}
+		else{
+			cout << *(--it) << "\n";
+			tickets.erase(it);
+		}
+	}
 }
-void sol()
-{
-}
-int main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    FAST;
-    int T = 1;
-    cin >> T;
-    while (T--)
-    {
-        input();
-        sol();
-    }
-    return 0;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	solve();
+	return 0;
 }
