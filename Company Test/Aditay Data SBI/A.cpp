3fdba72d8454111c6360e63e3203cab78937cdea
akash_ghadge <asghadge6@gmail.com>
2022-09-18 18:09:10 +0530

    Not test happend

---
+++ Company Test/Aditay Data SBI/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define en "\n"
+#define ll long long
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+
+}
