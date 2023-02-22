17cecb7ab98aa02c6dc9aed6b45abff6f932393b
akash_ghadge <asghadge6@gmail.com>
2023-02-22 18:30:50 +0530

    CF P Math G 1200 1 added

---
+++ CodeForces/Practice/Math-Group/1200/10 J. Yet Another Array Restoration.cpp
@@ -0,0 +1,293 @@
+/*
+Notes
+** ASCII of 'a'- 97,'z'- 123,'A'- 65,'Z'- 90,'0'- 48,'9'- 57
+*/
+/*
+We have a secret array. You don't know this array and you have to restore it. However, you know some facts about this array:
+
+The array consists of n
+ distinct positive (greater than 0
+) integers.
+The array contains two elements x
+ and y
+ (these elements are known for you) such that x<y
+.
+If you sort the array in increasing order (such that a1<a2<…<an
+), differences between all adjacent (consecutive) elements are equal (i.e. a2−a1=a3−a2=…=an−an−1)
+.
+It can be proven that such an array always exists under the constraints given below.
+
+Among all possible arrays that satisfy the given conditions, we ask you to restore one which has the minimum possible maximum element. In other words, you have to minimize max(a1,a2,…,an)
+.
+
+You have to answer t
+ independent test cases.
+
+Input
+The first line of the input contains one integer t
+ (1≤t≤100
+) — the number of test cases. Then t
+ test cases follow.
+
+The only line of the test case contains three integers n
+, x
+ and y
+ (2≤n≤50
+; 1≤x<y≤50
+) — the length of the array and two elements that are present in the array, respectively.
+
+Output
+For each test case, print the answer: n
+ integers a1,a2,…,an
+ (1≤ai≤109
+), where ai
+ is the i
+-th element of the required array. If there are several answers, you can print any (it also means that the order of elements doesn't matter).
+
+It can be proven that such an array always exists under the given constraints.
+
+Example
+inputCopy
+5
+2 1 49
+5 20 50
+6 20 50
+5 3 8
+9 13 22
+outputCopy
+1 49
+20 40 30 50 10
+26 32 20 38 44 50
+8 23 18 13 3
+1 10 13 4 19 22 25 16 7
+
+*/
+/*Basic Include */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Policy Based Data Structures*/
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define ordered_multiset tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+/* Functions*/
+#define mset(d, val) memset(d, val, sizeof(d))
+#define setp(x) cout << fixed << setprecision(x)
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define forn(i, a, b) for (int i = (a); i < (b); i++)
+#define forr(i, a, b) for (int i = (a); i >= (b); i--)
+#define fore(i, a, b) for (int i = (a); i <= (b); i++)
+#define prn(a) cout << a << endl
+#define prnsp(a) cout << a << " "
+#define prn2(a, b) cout << a << " " << b << endl
+#define prn3(a, b, c) cout << a << " " << b << " " << c << endl
+#define prn4(a, b, c, d) cout << a << " " << b << " " << c << " " << d << endl
+#define prnp(a) cout << a.first << " " << a.second << endl
+
+/* Inputs */
+#define var(n) \
+    ll n;      \
+    cin >> n;
+#define var2(a, b) \
+    ll a, b;       \
+    cin >> a >> b;
+#define var3(a, b, c) \
+    ll a, b, c;       \
+    cin >> a >> b >> c;
+#define var4(a, b, c, d) \
+    ll a, b, c, d;       \
+    cin >> a >> b >> c >> d;
+#define vars(s) \
+    string s;   \
+    cin >> s;
+#define varv(a, n)             \
+    vi a(n);                   \
+    for (ll i = 0; i < n; i++) \
+        cin >> a[i];
+#define varvvi(a, n, m)            \
+    vvi a(n, vi(m, 0));            \
+    for (ll i = 0; i < n; i++)     \
+        for (ll j = 0; j < m; j++) \
+            cin >> arr[i][j];
+#define varvpi(a, n)             \
+    vector<pair<int, int>> a(n); \
+    for (ll i = 0; i < n; i++)   \
+        cin >> a[i].first >> a[i].second;
+
+/* Shortcuts */
+#define pb push_back
+#define mkp make_pair
+#define F first
+#define S second
+#define fbo find_by_order // fbo(K) -> Kth element in a Set (counting from zero).
+#define ook order_of_key  // ook(K) -> Number of items strictly smaller than K.
+#define FAST()                        \
+    {                                 \
+        ios_base::sync_with_stdio(0); \
+        cin.tie(0);                   \
+    }
+#define en "\n"
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vvs;
+typedef vector<ii> vii;
+typedef vector<char> vch;
+typedef vector<string> vs;
+
+/*ARITHMETIC SECTION*/
+const ll INF = ll(1e18);
+const int mod = 1e9 + 7;
+ll binary_pow(ll x, ll y)
+{
+    ll res = 1;
+    while (y)
+    {
+        if (y % 2)
+            res = (res * x % mod) % mod;
+        x = (x * x) % mod;
+        y /= 2;
+    }
+    return res;
+}
+ll inv(ll x) { return binary_pow(x, mod - 2); }
+inline ll ceil_divide(ll a, ll b)
+{
+    if (a % b == 0)
+        return a / b;
+    else
+        return a / b + 1;
+}
+inline ll lcm(ll a, ll b) { return (a * b) / __gcd(a, b); }
+
+vi facts(ll n)
+{
+    vi ans;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            ans.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        ans.push_back(n);
+    return ans;
+}
+bool isPerSquare(ll a)
+{
+    if (a < 0)
+        return false;
+    ll sr = sqrt(a);
+    return (sr * sr == a);
+}
+
+// Modulo Operations
+ll add(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (((a + b) % mod) + mod) % mod;
+}
+ll mul(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (((a * b) % mod) + mod) % mod;
+}
+ll sub(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (((a - b) % mod) + mod) % mod;
+}
+ll m_div(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (mul(a, inv(b)) + mod) % mod;
+}
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+void prnv(auto b, auto e)
+{
+    for (auto i = b; i != e; i++)
+    {
+        cout << *i << " ";
+    }
+    cout << endl;
+}
+
+/******************************** Main Section Start *********************************/
+
+void pre_processing() {}
+void sol()
+{
+    var3(n, a, b);
+    if (n == 2)
+    {
+        prn2(a, b);
+    }
+    else
+    {
+        ll diff = b - a;
+        for (ll i = 1; i <= diff; i++)
+        {
+            vi ans;
+            bool f = 0;
+            ll ptr = b;
+            while (ptr > 0 and ans.size() < n)
+            {
+                if (ptr == a)
+                {
+                    f = 1;
+                }
+                ans.push_back(ptr);
+                ptr -= i;
+            }
+            if (f and ans.size() == n)
+            {
+                prnv(all(ans));
+                return;
+            }
+            else if (f and ans.size() < n)
+            {
+                ptr = b + i;
+                while (ans.size() != n)
+                {
+                    ans.push_back(ptr);
+                    ptr += i;
+                }
+                prnv(all(ans));
+                return;
+            }
+        }
+    }
+}
+
+/******************************** Main Section End *********************************/
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST();
+    ll TEST_CASE = 1;
+    cin >> TEST_CASE;
+    pre_processing();
+    forn(_, 0, TEST_CASE)
+    {
+        sol();
+    }
+    return 0;
+}
