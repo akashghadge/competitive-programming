b28eb2c14fba6a639ea10e86eed1d59d2a36d543
akash_ghadge <asghadge6@gmail.com>
2023-02-27 09:40:28 +0530

    CF PRACTICE MATH GROUP 1300 STARTED

---
+++ CodeForces/Practice/Math-Group/1300/04 D. Saving the City.cpp
@@ -0,0 +1,291 @@
+/*
+Bertown is a city with n
+ buildings in a straight line.
+
+The city's security service discovered that some buildings were mined. A map was compiled, which is a string of length n
+, where the i
+-th character is "1" if there is a mine under the building number i
+ and "0" otherwise.
+
+Bertown's best sapper knows how to activate mines so that the buildings above them are not damaged. When a mine under the building numbered x
+ is activated, it explodes and activates two adjacent mines under the buildings numbered x−1
+ and x+1
+ (if there were no mines under the building, then nothing happens). Thus, it is enough to activate any one mine on a continuous segment of mines to activate all the mines of this segment. For manual activation of one mine, the sapper takes a
+ coins. He can repeat this operation as many times as you want.
+
+Also, a sapper can place a mine under a building if it wasn't there. For such an operation, he takes b
+ coins. He can also repeat this operation as many times as you want.
+
+The sapper can carry out operations in any order.
+
+You want to blow up all the mines in the city to make it safe. Find the minimum number of coins that the sapper will have to pay so that after his actions there are no mines left in the city.
+
+Input
+The first line contains one positive integer t
+ (1≤t≤105
+) — the number of test cases. Then t
+ test cases follow.
+
+Each test case begins with a line containing two integers a
+ and b
+ (1≤a,b≤1000
+) — the cost of activating and placing one mine, respectively.
+
+The next line contains a map of mines in the city — a string consisting of zeros and ones.
+
+The sum of the string lengths for all test cases does not exceed 105
+.
+
+Output
+For each test case, output one integer — the minimum number of coins that the sapper will have to pay.
+
+Example
+inputCopy
+2
+1 1
+01000010
+5 1
+01101110
+outputCopy
+2
+6
+Note
+In the second test case, if we place a mine under the fourth building and then activate it, then all mines on the field are activated. The cost of such operations is six, b=1
+ coin for placing a mine and a=5
+ coins for activating.
+
+
+Codeforces (c) Copyright 2010-2023 Mike Mirzayanov
+
+*/
+/*
+Notes
+** ASCII of 'a'- 97,'z'- 123,'A'- 65,'Z'- 90,'0'- 48,'9'- 57
+*/
+
+/*Basic Include */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Policy Based Data Structures*/
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>
+#define ordered_multiset tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+/* Functions*/
+#define mset(d, val) memset(d, val, sizeof(d))
+#define setp(x) cout << fixed << setprecision(x)
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define forn(i, a, b) for (int i = (a); i < (b); i++)
+#define forr(i, a, b) for (int i = (a); i >= (b); i--)
+#define fore(i, a, b) for (int i = (a); i <= (b); i++)
+#define prn(a) cout << a << endl
+#define prnsp(a) cout << a << " "
+#define prn2(a, b) cout << a << " " << b << endl
+#define prn3(a, b, c) cout << a << " " << b << " " << c << endl
+#define prn4(a, b, c, d) cout << a << " " << b << " " << c << " " << d << endl
+#define prnp(a) cout << a.first << " " << a.second << endl
+
+/* Inputs */
+#define var(n) \
+    ll n;      \
+    cin >> n;
+#define var2(a, b) \
+    ll a, b;       \
+    cin >> a >> b;
+#define var3(a, b, c) \
+    ll a, b, c;       \
+    cin >> a >> b >> c;
+#define var4(a, b, c, d) \
+    ll a, b, c, d;       \
+    cin >> a >> b >> c >> d;
+#define vars(s) \
+    string s;   \
+    cin >> s;
+#define varv(a, n)             \
+    vi a(n);                   \
+    for (ll i = 0; i < n; i++) \
+        cin >> a[i];
+#define varvvi(a, n, m)            \
+    vvi a(n, vi(m, 0));            \
+    for (ll i = 0; i < n; i++)     \
+        for (ll j = 0; j < m; j++) \
+            cin >> arr[i][j];
+#define varvpi(a, n)             \
+    vector<pair<int, int>> a(n); \
+    for (ll i = 0; i < n; i++)   \
+        cin >> a[i].first >> a[i].second;
+
+/* Shortcuts */
+#define pb push_back
+#define mkp make_pair
+#define F first
+#define S second
+#define fbo find_by_order // fbo(K) -> Kth element in a Set (counting from zero).
+#define ook order_of_key  // ook(K) -> Number of items strictly smaller than K.
+#define FAST()                        \
+    {                                 \
+        ios_base::sync_with_stdio(0); \
+        cin.tie(0);                   \
+    }
+#define en "\n"
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vvs;
+typedef vector<ii> vii;
+typedef vector<char> vch;
+typedef vector<string> vs;
+
+/*ARITHMETIC SECTION*/
+const ll INF = ll(1e18);
+const int mod = 1e9 + 7;
+ll binary_pow(ll x, ll y)
+{
+    ll res = 1;
+    while (y)
+    {
+        if (y % 2)
+            res = (res * x % mod) % mod;
+        x = (x * x) % mod;
+        y /= 2;
+    }
+    return res;
+}
+ll inv(ll x) { return binary_pow(x, mod - 2); }
+inline ll ceil_divide(ll a, ll b)
+{
+    if (a % b == 0)
+        return a / b;
+    else
+        return a / b + 1;
+}
+inline ll lcm(ll a, ll b) { return (a * b) / __gcd(a, b); }
+
+vi facts(ll n)
+{
+    vi ans;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            ans.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        ans.push_back(n);
+    return ans;
+}
+bool isPerSquare(ll a)
+{
+    if (a < 0)
+        return false;
+    ll sr = sqrt(a);
+    return (sr * sr == a);
+}
+
+// Modulo Operations
+ll add(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (((a + b) % mod) + mod) % mod;
+}
+ll mul(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (((a * b) % mod) + mod) % mod;
+}
+ll sub(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (((a - b) % mod) + mod) % mod;
+}
+ll m_div(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (mul(a, inv(b)) + mod) % mod;
+}
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+void prnv(auto b, auto e)
+{
+    for (auto i = b; i != e; i++)
+    {
+        cout << *i << " ";
+    }
+    cout << endl;
+}
+
+/******************************** Main Section Start *********************************/
+
+void pre_processing() {}
+void sol()
+{
+    var2(a, b);
+    vars(str);
+    ll n = str.size();
+    bool first = 1;
+    ll prevz = 0;
+    ll ans = 0;
+    int i = 0;
+    while (i < n)
+    {
+        int j = i;
+        while (j < n and str[j] == '1')
+        {
+            j++;
+        }
+
+        if (j != i)
+        {
+            if (first)
+            {
+                ans += a;
+                first = 0;
+            }
+            else
+            {
+                ans += min((prevz * b), a);
+            }
+            prevz = 0;
+            i = j;
+        }
+        else
+        {
+            prevz++;
+            i++;
+        }
+        // prn3(i, first, ans);
+    }
+    prn(ans);
+}
+
+/******************************** Main Section End *********************************/
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST();
+    ll TEST_CASE = 1;
+    cin >> TEST_CASE;
+    pre_processing();
+    forn(_, 0, TEST_CASE)
+    {
+        sol();
+    }
+    return 0;
+}
