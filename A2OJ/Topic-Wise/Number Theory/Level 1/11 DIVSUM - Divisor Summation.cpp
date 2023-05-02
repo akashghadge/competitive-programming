415adbf8d38f03f08db7e1af9aa1c6b7f2fa42df
akash_ghadge <asghadge6@gmail.com>
2023-05-02 12:58:36 +0530

    A2OJ TW NT L1 1 added

---
+++ A2OJ/Topic-Wise/Number Theory/Level 1/11 DIVSUM - Divisor Summation.cpp
@@ -0,0 +1,192 @@
+/*
+Notes
+** ASCII of 'a'- 97,'z'- 123,'A'- 65,'Z'- 90,'0'- 48,'9'- 57
+*/
+
+/*Basic Include */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Functions*/
+#define mset(d, val) memset(d, val, sizeof(d))
+#define setp(x) cout << fixed << setprecision(x)
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define forn(i, a, b) for (int i = (a); i < (b); i++)
+#define forr(i, a, b) for (int i = (a); i >= (b); i--)
+#define fore(i, a, b) for (int i = (a); i <= (b); i++)
+#define prn(a) cout << a << endl
+#define prnsp(a) cout << a << " "
+#define prn2(a, b) cout << a << " " << b << endl
+#define prn3(a, b, c) cout << a << " " << b << " " << c << endl
+#define prn4(a, b, c, d) cout << a << " " << b << " " << c << " " << d << endl
+#define prnp(a) cout << a.first << " " << a.second << endl
+
+/* Inputs */
+#define var(n) \
+    ll n;      \
+    cin >> n;
+
+/* Shortcuts */
+#define pb push_back
+#define mkp make_pair
+#define F first
+#define S second
+#define fbo find_by_order // fbo(K) -> Kth element in a Set (counting from zero).
+#define ook order_of_key  // ook(K) -> Number of items strictly smaller than K.
+#define FAST()                        \
+    {                                 \
+        ios_base::sync_with_stdio(0); \
+        cin.tie(0);                   \
+    }
+#define en "\n"
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vvs;
+typedef vector<ii> vii;
+typedef vector<char> vch;
+typedef vector<string> vs;
+
+/*ARITHMETIC SECTION*/
+const ll INF = ll(1e18);
+const int mod = 1e9 + 7;
+ll binary_pow(ll x, ll y)
+{
+    ll res = 1;
+    while (y)
+    {
+        if (y % 2)
+            res = (res * x % mod) % mod;
+        x = (x * x) % mod;
+        y /= 2;
+    }
+    return res;
+}
+ll inv(ll x) { return binary_pow(x, mod - 2); }
+inline ll ceil_divide(ll a, ll b)
+{
+    if (a % b == 0)
+        return a / b;
+    else
+        return a / b + 1;
+}
+inline ll lcm(ll a, ll b) { return (a * b) / __gcd(a, b); }
+
+vi facts(ll n)
+{
+    vi ans;
+    for (ll i = 2; i * i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            ans.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 1)
+        ans.push_back(n);
+    return ans;
+}
+bool isPerSquare(ll a)
+{
+    if (a < 0)
+        return false;
+    ll sr = sqrt(a);
+    return (sr * sr == a);
+}
+
+// Modulo Operations
+ll add(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (((a + b) % mod) + mod) % mod;
+}
+ll mul(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (((a * b) % mod) + mod) % mod;
+}
+ll sub(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (((a - b) % mod) + mod) % mod;
+}
+ll m_div(ll a, ll b)
+{
+    a = a % mod;
+    b = b % mod;
+    return (mul(a, inv(b)) + mod) % mod;
+}
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+void prnv(auto b, auto e)
+{
+    for (auto i = b; i != e; i++)
+    {
+        cout << *i << " ";
+    }
+    cout << endl;
+}
+
+/******************************** Main Section Start *********************************/
+void dfs(vi &fact, ll id, ll curr, unordered_set<ll> &st)
+{
+    if (id == fact.size())
+    {
+        st.insert(curr);
+        return;
+    }
+    dfs(fact, id + 1, curr, st);
+    dfs(fact, id + 1, curr * fact[id], st);
+}
+void pre_processing() {}
+void sol()
+{
+    var(n);
+    if (n == 1)
+    {
+        prn(0);
+        return;
+    }
+    vi fact = facts(n);
+    if (fact.size() == 1)
+    {
+        prn(1);
+        return;
+    }
+    ll sum = 1;
+    unordered_set<ll> st;
+    dfs(fact, 0, 1, st);
+    for (auto val : st)
+    {
+        if (val != 1 and val != n)
+            sum += val;
+    }
+    prn(sum);
+}
+
+/******************************** Main Section End *********************************/
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    FAST();
+    ll TEST_CASE = 1;
+    cin >> TEST_CASE;
+    pre_processing();
+    forn(_, 0, TEST_CASE)
+    {
+        sol();
+    }
+    return 0;
+}
