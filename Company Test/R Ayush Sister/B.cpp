b7d3e61bd16c82f6ba8e8559e103109fcda28e11
akash_ghadge <asghadge6@gmail.com>
2024-03-18 16:09:13 +0530

    Chnage

---
+++ Company Test/R Ayush Sister/B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+class temp
+{
+    temp() {}
+    int areal, breal;
+    // void add(int a, int b)
+    // {
+    //     areal = a;
+    //     breal = b;
+    // }
+    void add(int areal, int breal)
+    {
+        areal = areal;
+        breal = areal;
+    }
+};
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    // complex c1;
+    string str = "akash";
+    // cout << str.length() << endl;
+    // cout << str.size() << endl;
+    /*
+    Base -> ptr
+    |
+    Child
+    Child c;
+    Base b;
+    *ptr=b;
+    *ptr=c;
+    */
+}
