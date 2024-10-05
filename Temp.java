2ca14f145482fb83e72d49e1068b4e02a3258de6
akash_ghadge <asghadge6@gmail.com>
2024-10-05 21:52:32 +0530

    CP Div2 1 added

---
+++ Temp.java
@@ -0,0 +1,26 @@
+
+/**
+ * Temp
+ */
+import java.util.Scanner;
+
+public class Temp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b=sc.nextLine();
+        if(a=="tiger" && b=="ti")
+        {
+            System.out.println("ger");
+        }
+        else if(a=="processed" && b=="esd")
+        {
+            System.out.println("proc");
+        }
+        else if(a=="talent" && b=="tens")
+        {
+            System.out.println("al");
+        }
+    }
+}
